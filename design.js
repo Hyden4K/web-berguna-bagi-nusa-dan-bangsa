41848537e5d2af93d3cf231344aa870f1ac510e3
Hyden4K <bagas.akbar.f@gmail.com>
2023-05-14 20:55:16 +0700

    p

--- design.js
+++
@@ -1,6 +0,0 @@
-var menu = document.querySelector('nav ul');
-
-document.querySelector('.menu-toggle').addEventListener('click', function(e) {
-  e.preventDefault();
-  menu.classList.toggle('show');
-});
